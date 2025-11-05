dc1930337f3387f22603c72fea48e8d59cfc4ee0
Surge4315 <116834383+Surge4315@users.noreply.github.com>
2025-11-06 00:19:27 +0100

    platformio version with daylight saving changing
    auto changes between daylight saving zones for US and EU

---
+++ main.cpp
@@ -0,0 +1,226 @@
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <Wire.h>
+#include <WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <time.h>
+
+// Piny do I2C OLED
+#define SCREEN_SDA 21
+#define SCREEN_SCL 22
+
+const int buttonPin = 0;
+int hour = 3600;
+int offset = 2 * hour; // base time offset GMT+2 for Cracow
+
+volatile int clockCounter = 0;
+
+volatile bool buttonPressed = false;
+volatile unsigned long lastInterruptTime = 0;
+const unsigned long debounceDelay = 200; // 200 ms
+
+// WiFi
+const char *ssid = "";
+const char *password = "";
+
+// OLED
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, SCREEN_SCL, SCREEN_SDA);
+
+// NTP
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", 0, 3600000);
+
+// last sunday of march and october
+bool EUSummerTime(int day, int month, int wday)
+{
+    // wday: 0 = sunday, 1 = monday, ..., 6 = saturday
+    if (month < 3 || month > 10)
+        return false; // january–februaru and november–december -> winter time
+    if (month > 3 && month < 10)
+        return true; // april–september -> summer time
+
+    //looking for last sunday of march or october
+    int lastSunday;
+    if (month == 3)
+    {
+        // last march sunday
+        lastSunday = 31 - ((5 + wday) % 7);
+        return (day >= lastSunday);
+    }
+    else
+    {
+        // last october sunday
+        lastSunday = 31 - ((5 + wday) % 7);
+        return !(day >= lastSunday);
+    }
+}
+
+// second sunday of march to first sunday of november
+bool USSummerTime(int day, int month, int wday)
+{
+    // wday: 0 = sunday, 1 = monday, ..., 6 = saturday
+    if (month < 3 || month > 11)
+        return false; // January–February and December -> standard time
+    if (month > 3 && month < 11)
+        return true; // April–October -> daylight time
+
+    int firstSunday, secondSunday;
+
+    if (month == 3)
+    {
+        // find weekday of March 1 based on given day and weekday
+        int march1_wday = (wday - ((day - 1) % 7) + 7) % 7;
+
+        // first Sunday of March
+        firstSunday = (march1_wday == 0) ? 1 : (8 - march1_wday);
+        // second Sunday
+        secondSunday = firstSunday + 7;
+
+        return (day >= secondSunday);
+    }
+    else // month == 11
+    {
+        // find weekday of November 1
+        int nov1_wday = (wday - ((day - 1) % 7) + 7) % 7;
+        // first Sunday of November
+        firstSunday = (nov1_wday == 0) ? 1 : (8 - nov1_wday);
+
+        return !(day >= firstSunday);
+    }
+}
+
+
+// interrupt handler for button press with debounce
+void IRAM_ATTR handleButtonPress()
+{
+    unsigned long currentTime = millis();
+    if (currentTime - lastInterruptTime > debounceDelay)
+    {
+        buttonPressed = true;
+        lastInterruptTime = currentTime;
+    }
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    Wire.begin(SCREEN_SDA, SCREEN_SCL);
+    pinMode(buttonPin, INPUT_PULLUP);
+
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(1000);
+        Serial.println("Connecting to WiFi...");
+    }
+    Serial.println("Connected to WiFi");
+
+    u8g2.begin();
+    timeClient.begin();
+
+    attachInterrupt(digitalPinToInterrupt(buttonPin), handleButtonPress, RISING);
+}
+
+void loop()
+{
+
+    // first time getting for time offset calculation
+    unsigned long currentEpoch = timeClient.getEpochTime();
+    time_t currentTime = (time_t)currentEpoch;
+    struct tm *timeInfo = localtime(&currentTime);
+
+    int EUtime = 0;
+    if (EUSummerTime(timeInfo->tm_mday, timeInfo->tm_mon + 1, timeInfo->tm_wday))
+    {
+        EUtime = 0; // if summertime than no change
+    }
+    else
+    {
+        EUtime = hour; // if no summertime then we subtract one hour
+    }
+    int UStime = 0;
+    if (USSummerTime(timeInfo->tm_mday, timeInfo->tm_mon + 1, timeInfo->tm_wday))
+    {
+        UStime = 0; // if summertime time than no change
+    }
+    else
+    {
+        UStime = hour; // if no summertime time then we subtract one hour
+    }
+
+    if (buttonPressed)
+    {
+        buttonPressed = false; // flag reset
+
+        clockCounter = (clockCounter + 1) % 7;
+    }
+
+    switch (clockCounter)
+    {
+    case 0:
+        offset = 2 * hour - EUtime; // Cracow
+        break; 
+    case 1:
+        offset = 3 * hour - EUtime; // Pitesti
+        break; 
+    case 2:
+        offset = 8 * hour; // Manila
+        break; 
+    case 3:
+        offset = 9 * hour; // Tokyo
+        break; 
+    case 4:
+        offset = -7 * hour - UStime; // Los Angeles
+        break; 
+    case 5:
+        offset = -5 * hour - UStime;// New Orleans
+        break; 
+    case 6:
+        offset = hour - EUtime; // London
+        break; 
+    default:
+        offset = 0; // Greenwich Mean Time
+        break; 
+    }
+
+    timeClient.setTimeOffset(offset);
+    timeClient.update(); //updates time after set interval
+
+    // second time getting for proper display
+    currentEpoch = timeClient.getEpochTime();
+    currentTime = (time_t)currentEpoch;
+    timeInfo = localtime(&currentTime);
+
+    int hours = timeInfo->tm_hour;
+    int minutes = timeInfo->tm_min;
+    int seconds = timeInfo->tm_sec;
+    int day = timeInfo->tm_mday;
+    int month = timeInfo->tm_mon + 1;
+    int year = timeInfo->tm_year + 1900;
+
+    String daysOfWeek[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+    String locations[] = {"Cracow", "Pitesti", "Manila", "Tokyo", "Los Angeles", "New Orleans", "London"};
+    String dayOfWeek = daysOfWeek[timeInfo->tm_wday];
+    String location = locations[clockCounter];
+
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_ncenB18_tr);
+    String timeStr = String(hours) + ":" + (minutes < 10 ? "0" : "") + String(minutes) + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+    int textWidth = u8g2.getStrWidth(timeStr.c_str());
+    u8g2.setCursor((128 - textWidth) / 2, 24);
+    u8g2.print(timeStr);
+
+    u8g2.setFont(u8g2_font_ncenB10_tr);
+    String dateStr = (day < 10 ? "0" : "") + String(day) + "/" + (month < 10 ? "0" : "") + String(month) + "/" + String(year) + " " + dayOfWeek;
+    int dateTextWidth = u8g2.getStrWidth(dateStr.c_str());
+    u8g2.setCursor((128 - dateTextWidth) / 2, 40);
+    u8g2.print(dateStr);
+
+    int cityWidth = u8g2.getStrWidth(location.c_str());
+    u8g2.setCursor((128 - cityWidth) / 2, 56);
+    u8g2.print(location);
+
+    u8g2.sendBuffer();
+    delay(100);
+}
